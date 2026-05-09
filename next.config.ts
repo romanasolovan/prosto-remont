7204a47f5d3eb29a32e8b4bef46d08898c4b584c
Romana Solovan <solo.rv95@gmail.com>
2026-05-09 14:41:17 +0200

    install Payload and dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+import { withPayload } from "@payloadcms/next/withPayload";
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
@@ -5,4 +6,4 @@ const nextConfig: NextConfig = {};
 
 const withNextIntl = createNextIntlPlugin();
 
-export default withNextIntl(nextConfig);
+export default withPayload(withNextIntl(nextConfig));
