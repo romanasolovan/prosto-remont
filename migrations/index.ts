c43c3190de58155ef1d04d0d7d584b248fbb2adc
Romana Solovan <solo.rv95@gmail.com>
2026-05-11 15:13:59 +0200

    add: initial database migration for all collections

---
+++ migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260511_131234 from './20260511_131234';
+
+export const migrations = [
+  {
+    up: migration_20260511_131234.up,
+    down: migration_20260511_131234.down,
+    name: '20260511_131234'
+  },
+];
